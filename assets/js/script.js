f7a0ef32dd551e0fd7d83399530387c3c0403023
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-11 23:37:38 +1100

    Modify 'script.js' to add pseudocode for web application functionality

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// DISPLAY CURRENT DATE + DAY BELOW HEADING
+// moment().format('dddd, MMMM Do, YYYY'))
+
+
+// COLOR-CODE EACH TIMEBLOCK TO INDICATE WHETHER IT'S IN THE PAST, PRESENT OR FUTURE
+// if (moment().format('H')) === div.id) {div background: color-present}
+// else if (moment().format('H')) < div.id) {div background: color-future}
+// else if (moment().format('H')) > div.id) {div background: color-past}
+
+
+// WHEN TIMEBLOCK IS CLICKED, USER CAN ENTER TEXT
+// add click event to timeblocks so that when they are clicked, create new textarea element
+
+
+// WHEN SAVE BUTTON IS CLICKED WHEN THERE IS TEXT IN THE TIMEBLOCK, IT WILL BE SAVED IN LOCAL STORAGE
+// add click event to save buttons so that when they are clicked, textarea value is saved to local storage
