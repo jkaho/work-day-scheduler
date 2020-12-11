8658476cc7beeb73ec9c7827296b8324d77493e1
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-11 23:50:34 +1100

    Edit 'index.html' and 'script.js' pseudocode in reference to existing css styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,13 @@
 
 
 // COLOR-CODE EACH TIMEBLOCK TO INDICATE WHETHER IT'S IN THE PAST, PRESENT OR FUTURE
-// if (moment().format('H')) === div.id) {div background: color-present}
-// else if (moment().format('H')) < div.id) {div background: color-future}
-// else if (moment().format('H')) > div.id) {div background: color-past}
+// if (moment().format('H')) === div.id) {class: present}
+// else if (moment().format('H')) < div.id) {class: future}
+// else if (moment().format('H')) > div.id) {class: past}
 
 
 // WHEN TIMEBLOCK IS CLICKED, USER CAN ENTER TEXT
-// add click event to timeblocks so that when they are clicked, create new textarea element
+// html textarea element
 
 
 // WHEN SAVE BUTTON IS CLICKED WHEN THERE IS TEXT IN THE TIMEBLOCK, IT WILL BE SAVED IN LOCAL STORAGE
